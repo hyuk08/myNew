e32020c6b61c2bb7dd0852cd4685d8afb7be3def
hyuk08 <cmo020824@gmail.com>
2024-11-12 14:39:11 +0900

    1112_done

--- sketch.js
+++ sketch.js
@@ -1,277 +1,64 @@
-// The snake moves along a grid, one space at a time
-// The grid is smaller than the canvas, and its dimensions
-//  are stored in these variables
-let gridWidth = 30;
-let gridHeight = 30;
-
-let gameStarted = false;
-
-// How many segments snake starts with
-let startingSegments = 10;
-
-// Starting coordinates for first segment
-let xStart = 0;
-let yStart = 15;
-
-// Starting direction of motion
-let startDirection = 'right';
-
-// Current direction of motion
-let direction = startDirection;
-
-// The snake is divided into small segments,
-// stored as vectors in this array
-let segments = [];
-
-let score = 0;
-let highScore;
-
-// The fruit's position is stored as a vector
-// in this variable
-let fruit;
+let system;
 
 function setup() {
-  createCanvas(500, 500);
-
-  // Adjust frame rate to set movement speed
-  frameRate(10);
-
-  textAlign(CENTER, CENTER);
-  textSize(2);
-
-  // Check for saved high score in local browser storage
-  // If no score has been stored, this will be undefined
-  highScore = getItem('high score');
-
-  describe(
-    'A reproduction of the arcade game Snake, in which a snake, represented by a green line on a black background, is controlled by the arrow keys. Users move the snake toward a fruit, represented by a red dot, but the snake must not hit the sides of the window or itself.'
-  );
+  createCanvas(720, 400);
+  system = new ParticleSystem(createVector(width / 2, 50));
 }
 
 function draw() {
-  background(0);
-
-  // Set scale so that the game grid fills canvas
-  scale(width / gridWidth, height / gridHeight);
-  if (gameStarted === false) {
-    showStartScreen();
-  } else {
-    // Shift over so that snake and fruit are still on screen
-    // when their coordinates are 0
-    translate(0.5, 0.5);
-    showFruit();
-    showSegments();
-    updateSegments();
-    checkForCollision();
-    checkForFruit();
-  }
+  background(51);
+  system.addParticle();
+  system.run();
 }
 
-function showStartScreen() {
-  noStroke();
-  fill(32);
-  rect(2, gridHeight / 2 - 5, gridWidth - 4, 10, 2);
-  fill(255);
-  text(
-    'Click to play.\nUse arrow keys to move.',
-    gridWidth / 2,
-    gridHeight / 2
-  );
-  noLoop();
-}
+// 간단한 파티클 클래스
+let Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.copy();
+  this.lifespan = 255;
+};
 
-function mousePressed() {
-  if (gameStarted === false) {
-    startGame();
-  }
-}
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
 
-function startGame() {
-  // Put the fruit in a random place
-  updateFruitCoordinates();
+// 위치 업데이트를 위한 메소드
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
 
-  // Start with an empty array for segments
-  segments = [];
+// 화면에 보이기 위한 메소드
+Particle.prototype.display = function() {
+  stroke(200, this.lifespan);
+  strokeWeight(2);
+  fill(127, this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
 
-  // Start with x at the starting position and repeat until specified
-  // number of segments have been created, increasing x by 1 each time
-  for (let x = xStart; x < xStart + startingSegments; x += 1) {
-    // Create a new vector at the current position
-    let segmentPosition = createVector(x, yStart);
+// 파티클이 여전히 쓸만한가요?
+Particle.prototype.isDead = function(){
+  return this.lifespan < 0;
+};
 
-    // Add it to the beginning of the array
-    segments.unshift(segmentPosition);
-  }
+let ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
 
-  direction = startDirection;
-  score = 0;
-  gameStarted = true;
-  loop();
-}
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
 
-function showFruit() {
-  stroke(255, 64, 32);
-  point(fruit.x, fruit.y);
-}
-
-function showSegments() {
-  noFill();
-  stroke(96, 255, 64);
-  beginShape();
-  for (let segment of segments) {
-    vertex(segment.x, segment.y);
-  }
-  endShape();
-}
-
-function updateSegments() {
-  // Remove last segment
-  segments.pop();
-
-  // Copy current head of snake
-  let head = segments[0].copy();
-
-  // Insert the new snake head at the beginning of the array
-  segments.unshift(head);
-
-  // Adjust the head's position based on the current direction
-  switch (direction) {
-    case 'right':
-      head.x = head.x + 1;
-      break;
-    case 'up':
-      head.y = head.y - 1;
-      break;
-    case 'left':
-      head.x = head.x - 1;
-      break;
-    case 'down':
-      head.y = head.y + 1;
-      break;
-  }
-}
-
-function checkForCollision() {
-  // Store first segment in array as head
-  let head = segments[0];
-
-  // If snake's head...
-  if (
-    // hit right edge or
-    head.x >= gridWidth ||
-    // hit left edge or
-    head.x < 0 ||
-    // hit bottom edge or
-    head.y >= gridHeight ||
-    // hit top edge or
-    head.y < 0 ||
-    // collided with itself
-    selfColliding() === true
-  ) {
-    // show game over screen
-    gameOver();
-  }
-}
-
-function gameOver() {
-  noStroke();
-  fill(32);
-  rect(2, gridHeight / 2 - 5, gridWidth - 4, 12, 2);
-  fill(255);
-
-  // Set high score to whichever is larger: current score or previous
-  // high score
-  highScore = max(score, highScore);
-
-  // Put high score in local storage. This will be be stored in browser
-  // data, even after the user reloads the page.
-  storeItem('high score', highScore);
-  text(
-    `Game over!
-Your score: ${score}
-High score: ${highScore}
-Click to play again.`,
-    gridWidth / 2,
-    gridHeight / 2
-  );
-  gameStarted = false;
-  noLoop();
-}
-
-function selfColliding() {
-  // Store the last segment as head
-  let head = segments[0];
-
-  // Store every segment except the first
-  let segmentsAfterHead = segments.slice(1);
-
-  // Check each of the other segments
-  for (let segment of segmentsAfterHead) {
-    // If segment is in the same place as head
-    if (segment.equals(head) === true) {
-      return true;
+ParticleSystem.prototype.run = function() {
+  for (let i = this.particles.length-1; i >= 0; i--) {
+    let p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
     }
   }
-  return false;
-}
-
-function checkForFruit() {
-  // Store first segment as head
-  let head = segments[0];
-
-  // If the head segment is in the same place as the fruit
-  if (head.equals(fruit) === true) {
-    // Give player a point
-    score = score + 1;
-
-    // Duplicate the tail segment
-    let tail = segments[segments.length - 1];
-    let newSegment = tail.copy();
-
-    // Put the duplicate in the beginning of the array
-    segments.push(newSegment);
-
-    // Reset fruit to a new location
-    updateFruitCoordinates();
-  }
-}
-
-function updateFruitCoordinates() {
-  // Pick a random new coordinate for the fruit
-  // and round it down using floor().
-  // Because the segments move in increments of 1,
-  // in order for the snake to hit the same position
-  // as the fruit, the fruit's coordinates must be
-  // integers, but random() returns a float
-  let x = floor(random(gridWidth));
-  let y = floor(random(gridHeight));
-  fruit = createVector(x, y);
-}
-
-// When an arrow key is pressed, switch the snake's direction of movement,
-// but if the snake is already moving in the opposite direction,
-// do nothing.
-function keyPressed() {
-  switch (keyCode) {
-    case LEFT_ARROW:
-      if (direction !== 'right') {
-        direction = 'left';
-      }
-      break;
-    case RIGHT_ARROW:
-      if (direction !== 'left') {
-        direction = 'right';
-      }
-      break;
-    case UP_ARROW:
-      if (direction !== 'down') {
-        direction = 'up';
-      }
-      break;
-    case DOWN_ARROW:
-      if (direction !== 'up') {
-        direction = 'down';
-      }
-      break;
-  }
-}
+};
